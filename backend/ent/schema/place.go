33c5417e76a4186517a2dca4826bb78f6cf24cb0
TOEILAKKHANA <lkn.7oei@gmail.com>
2020-12-21 02:06:52 +0700

    ทำ entity ของสถานที่จัดกิจกรรม - close #43

---
+++ backend/ent/schema/place.go
@@ -0,0 +1,24 @@
+package schema
+
+import (
+	"github.com/facebookincubator/ent"
+	"github.com/facebookincubator/ent/schema/field"
+)
+
+// Place holds the schema definition for the Place entity.
+type Place struct {
+	ent.Schema
+}
+
+// Fields of the Place.
+func (Place) Fields() []ent.Field {
+	return []ent.Field{
+
+		field.String("PLACE").NotEmpty(),
+	}
+}
+
+// Edges of the Place.
+func (Place) Edges() []ent.Edge {
+	return nil
+}
