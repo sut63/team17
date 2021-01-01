a1eec92dcfdb215f800df8ae88179ffb037f1952
ht101010 <hunter0darkness@gmail.com>
2021-01-01 21:40:12 +0700

    ออกแบบ gender entity - close #47

--- backend/ent/schema/student.go
+++ backend/ent/schema/student.go
@@ -1,6 +1,8 @@
 package schema
 
 import "github.com/facebookincubator/ent"
+import "github.com/facebookincubator/ent/schema/field"
+import "github.com/facebookincubator/ent/schema/edge"
 
 // Student holds the schema definition for the Student entity.
 type Student struct {
@@ -10,17 +12,23 @@ type Student struct {
 // Fields of the Student.
 func (Student) Fields() []ent.Field {
 	return []ent.Field{
-		field.String("student_id").NotEmpty(),
 		field.String("fname").NotEmpty(),
 		field.String("lname").NotEmpty(),
 		field.String("institution").NotEmpty(),
 		field.String("recent_address").NotEmpty(),
-		field.Int("telephone").NotEmpty(),
-		field.String("email").NotEmpty(),
+		field.Int("telephone"),
+		field.String("email"),
 	}
 }
 
 // Edges of the Student.
 func (Student) Edges() []ent.Edge {
-	return nil
+	return []ent.Edge{
+		edge.From("stud_gend", Gender.Type).Ref("gend_stud").Unique(),
+		edge.To("stud_acti", Activity.Type),
+		edge.From("stud_prov", Province.Type).Ref("prov_stud").Unique(),
+		edge.To("stud_resu", Result.Type),
+		edge.From("stud_pref", Prefix.Type).Ref("pref_stud").Unique(),
+		edge.From("stud_degr", Degree.Type).Ref("degr_stud").Unique(),
+	}
 }
