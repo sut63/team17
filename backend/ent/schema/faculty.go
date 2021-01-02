7a0a1d15b75bf3e3f4997ff8aa7a68fa42bc7be5
Jeeninee <Jeeninee.sai@gmail.com>
2021-01-02 18:44:00 +0700

    แก้ไข entity Faculty

---
+++ backend/ent/schema/faculty.go
@@ -0,0 +1,29 @@
+package schema
+
+import (
+		"github.com/facebookincubator/ent"
+		"github.com/facebookincubator/ent/schema/edge"
+		"github.com/facebookincubator/ent/schema/field"
+)
+
+// Faculty holds the schema definition for the Faculty entity.
+
+type Faculty struct {
+	ent.Schema
+}
+
+// Fields of the Faculty.
+func (Faculty) Fields() []ent.Field {
+	return []ent.Field{
+		
+		field.String("faculty").NotEmpty(),
+	}
+}
+
+// Edges of the Faculty.
+func (Faculty) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("fac_course", Course.Type),
+		
+	}
+}
