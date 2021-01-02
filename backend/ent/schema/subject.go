05aa273c4bf058789f4653406b5cd1a6c6fce46e
Narong Sirima game <graziellagame@hotmail.com>
2021-01-02 16:06:26 +0700

    ทำ entity Subject ของระบบประวัติผลการศึกษา - close #63

---
+++ backend/ent/schema/subject.go
@@ -0,0 +1,28 @@
+package schema
+
+import (
+	"github.com/facebookincubator/ent"
+	"github.com/facebookincubator/ent/schema/edge"
+	"github.com/facebookincubator/ent/schema/field"
+)
+
+// Subject holds the schema definition for the Subject entity.
+type Subject struct {
+	ent.Schema
+}
+
+// Fields of the Subject.
+func (Subject) Fields() []ent.Field {
+	return []ent.Field{
+		field.Int("code").Positive(),
+		field.String("subjects").NotEmpty(),
+		field.Int("creditpiont").Positive(),
+	}
+}
+
+// Edges of the Subject.
+func (Subject) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("subject1", Results.Type),
+	}
+}
