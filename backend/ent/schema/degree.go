a371bbd67fc9786a575c924db551b7a1ed650d0b
Jeeninee <Jeeninee.sai@gmail.com>
2021-01-02 16:06:47 +0700

    ออกแบบ entity ชื่อ degree - close #61

---
+++ backend/ent/schema/degree.go
@@ -0,0 +1,29 @@
+package schema
+
+import (
+		"github.com/facebookincubator/ent"
+		"github.com/facebookincubator/ent/schema/edge"
+		"github.com/facebookincubator/ent/schema/field"
+)
+
+// Degree holds the schema definition for the  Degree entity.
+
+type  Degree struct {
+	ent.Schema
+}
+
+// Fields of the  Degree.
+func (Degree) Fields() []ent.Field {
+	return []ent.Field{
+		
+		field.String("degree").NotEmpty(),
+	}
+}
+
+// Edges of the  Degree.
+func (Degree) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("course", Course.Type),
+		
+	}
+}
