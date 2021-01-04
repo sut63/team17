936dc5d0ab70b74a242788d14691977810ae6779
Jeeninee <Jeeninee.sai@gmail.com>
2021-01-04 12:15:38 +0700

    แก้ไขชื่อใน course close #69

--- backend/ent/schema/course.go
+++ backend/ent/schema/course.go
@@ -1,10 +1,10 @@
 package schema
 
 import (
-		"github.com/facebookincubator/ent"
-		"github.com/facebookincubator/ent/schema/edge"
-		"github.com/facebookincubator/ent/schema/field")
-
+	"github.com/facebookincubator/ent"
+	"github.com/facebookincubator/ent/schema/edge"
+	"github.com/facebookincubator/ent/schema/field"
+)
 
 // Course holds the schema definition for the Course entity.
 type Course struct {
@@ -14,18 +14,18 @@ type Course struct {
 // Fields of the Course.
 func (Course) Fields() []ent.Field {
 	return []ent.Field{
-        field.String("course").NotEmpty(),
-    }
+		field.String("course").NotEmpty(),
+	}
 }
 
 // Edges of the Course.
 func (Course) Edges() []ent.Edge {
 	return []ent.Edge{
-		edge.From("course_fac", Faculty.Type).
-			Ref("fac_course").Unique(),
-		edge.From("course_deg", Degree.Type).
-			Ref("deg_course").Unique(),
-		edge.From("course_ins", Institution.Type).
-			Ref("ins_course").Unique(),
-    }
+		edge.From("cour_facu", Faculty.Type).
+			Ref("facu_cour").Unique(),
+		edge.From("cour_degr", Degree.Type).
+			Ref("degr_cour").Unique(),
+		edge.From("cour_inst", Institution.Type).
+			Ref("inst_cour").Unique(),
+	}
 }
